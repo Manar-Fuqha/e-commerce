a3f731fc981b934d117d481f7c9b822c800e4230
Manar-Fuqha <manar0fuqha2002@gmail.com>
2025-12-19 21:56:53 +0300

    add server validation for send code page

--- src/pages/sendCode/SendCode.jsx
+++ src/pages/sendCode/SendCode.jsx
@@ -3,29 +3,39 @@ import { useForm } from "react-hook-form"
 import axios from "axios";
 import { SendCodeSchema } from "../../validations/SendCodeSchema";
 import {yupResolver } from "@hookform/resolvers/yup"
+import { useState } from "react";
+
 
 
 export default function SendCode() {
+    const [serverErrors, setServerErrors] =useState([]);
     const {register , handleSubmit , formState:{errors}} =useForm({
         resolver:yupResolver(SendCodeSchema),
         mode:'onBlur'
       })
     const sendCodeForm =async(values)=>{
         try{
-            await axios.post(`https://knowledgeshop.runasp.net/api/Auth/Account/SendCode`,values);
+           const response= await axios.post(`https://knowledgeshop.runasp.net/api/Auth/Account/SendCode`,values);
+           console.log(response.data.message)
         }
         catch(err){
-            console.log(err)
+            console.log(err) 
+            setServerErrors([err.response.data.message])
         }
     }
 
   return (
     <Box sx={{display:'flex' , flexDirection:'column' , alignItems:'center' ,justifyContent:'center' , minHeight:'100vh' }}>
-        <Typography sx={{ textTransform: "uppercase" , color:'#3a3a3a'}} variant="" component="h1">Reset your password</Typography>
-        <Typography sx={{pt:'20px' ,color:'#3a3a3a' , fontSize:'18px', fontWeight:'400' , fontFamily:'Helvetica,Arial,sans-serif'}} variant="" component={"p"}>We will send you an email to reset your password.</Typography>
+        <Typography sx={{ textTransform: "uppercase" , color:'#3a3a3a'}}  component="h1">Reset your password</Typography>
+        <Typography sx={{pt:'20px' ,color:'#3a3a3a' , fontSize:'18px', fontWeight:'400' , fontFamily:'Helvetica,Arial,sans-serif'}}  component={"p"}>We will send you an email to reset your password.</Typography>
         
         <Container maxWidth='sm'>
-            <Box onSubmit={handleSubmit(sendCodeForm)} variant component={'form'} sx={{pt:'20px' }}>
+            {serverErrors.length >0 ?
+                serverErrors.map( (err)=>
+                <Typography sx={{color:"red"  , fontWeight:'bold' , y:'20px'}}>{err}</Typography>
+                )
+                :null}
+            <Box onSubmit={handleSubmit(sendCodeForm)}  component={'form'} sx={{pt:'20px' }}>
                 <TextField {...register('email')}  label="Email" fullWidth variant="outlined" 
                 error={errors.email} helperText={errors.email?.message}/>
                 <Button type='submit' variant="contained" sx={{p:'12px', backgroundColor:'#3a3a3a',textTransform:'uppercase' , mt:'20px' , display:'flex' , alignItems:'center' , mx:'auto' , letterSpacing:'3px'}}>submit</Button>
