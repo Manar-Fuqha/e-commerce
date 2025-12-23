0321543c57324c51c3a3ca83eba33f27a66d1cb5
Manar-Fuqha <manar0fuqha2002@gmail.com>
2025-12-23 15:59:05 +0300

    add navbar design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import { Box, Link, Typography } from '@mui/material'
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+
+ import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
 
 export default function Navbar() {
+  
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: 25,
+  backgroundColor :' rgb(230, 230, 230)',
+  // '&:hover': {
+  //   backgroundColor: alpha(theme.palette.common.white, 0.25),
+  // },
+  marginLeft: 0,
+  width: '100%',
+  
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '30ch',
+    },
+  },
+}));
+
   return (
-    <div>
-      Navbar
-    </div>
+    <Box sx={{display:'flex', py:2 , justifyContent:'space-between' , alignItems:'center' , px:'10px'}}>
+      <Typography sx={{flexGrow:'1' ,fontWeight:'bold'}} variant='h5' component={'h1'}>Picksy</Typography> 
+
+
+    <Box sx={{ flexGrow:'2', bgcolor:'gray' , borderRadius:5}}>
+      
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="What Are You Looking For?"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          
+    </Box>
+  
+
+      <Box sx={{display:'flex' ,flexGrow:'1' , justifyContent:'flex-end' , gap:3}}>
+        <Link href="/auth/login">
+         <svg    style={{cursor:'pointer'}}  xmlns="http://www.w3.org/2000/svg" height='25' width='25'  viewBox="0 0 640 640"><path fill="#000000" d="M240 192C240 147.8 275.8 112 320 112C364.2 112 400 147.8 400 192C400 236.2 364.2 272 320 272C275.8 272 240 236.2 240 192zM448 192C448 121.3 390.7 64 320 64C249.3 64 192 121.3 192 192C192 262.7 249.3 320 320 320C390.7 320 448 262.7 448 192zM144 544C144 473.3 201.3 416 272 416L368 416C438.7 416 496 473.3 496 544L496 552C496 565.3 506.7 576 520 576C533.3 576 544 565.3 544 552L544 544C544 446.8 465.2 368 368 368L272 368C174.8 368 96 446.8 96 544L96 552C96 565.3 106.7 576 120 576C133.3 576 144 565.3 144 552L144 544z"/></svg>
+      </Link>
+          <ShoppingBagIcon />
+      
+      </Box>
+     
+    </Box>
   )
 }
